dcdcd0b9b467cdec9e3732d62482fb2747c1de57
itsandeepv <114796141+itsandeepv@users.noreply.github.com>
2022-11-12 11:21:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Offers from "./pages/Offers";
+import ForgetPassword from "./pages/ForgetPassword";
+import Header from "./components/Header";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import PrivateRouter from "./components/PrivateRouter";
+import CreateListing from "./pages/CreateListing";
+function App() {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<PrivateRouter />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/create-listing" element={<PrivateRouter />}>
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/forget-password" element={<ForgetPassword />} />
+        </Routes>
+      </Router>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </>
+  );
+}
+
+export default App;
